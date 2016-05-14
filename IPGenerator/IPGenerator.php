859f4f1383cb5ad4793c4fba04b1ec404b9fe05f
pinzon1992 <juanpinzon1992@gmail.com>
2016-05-14 16:52:13 -0500

    Code Documentation
    Agregada documentacion del codigo

--- IPGenerator/IPGenerator.php
+++ IPGenerator/IPGenerator.php
@@ -2,6 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class IPGenerator {
+  /*
+  * @param  string  $inicio Una direccion IPv4 valida para iniciar el rango
+  * @param  string  $fin Una direccion IPv4 valida para finalizar el rango
+  * @return array  Un arreglo que contiene las direcciones ip del rango dado
+  */
   public function ipRange($inicio, $fin){
     $inicio = ip2long($inicio);
     $fin = ip2long($fin);
